e52adc33bac39c7953827649b20a6897884e97a2
Dung Ly <tobidung@gmail.com>
2021-05-16 19:07:55 +0700

    added unit tests for controller

--- test/todo_test.dart
+++ test/todo_test.dart
@@ -5,8 +5,32 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:todoapp/controllers/todoController.dart';
+import 'package:todoapp/models/todoModel.dart';
 
 void main() {
-  test("able to check todo", () {});
+  test("able to check todo", () {
+    var todoController = TodoController();
+    todoController.todoList
+        .add(TodoModel(name: "Test 01", id: 0, isComplete: false));
+    todoController.checkTodo(0, true);
+    expect(todoController.todoList[0].isComplete, true);
+  });
+  test("able to add todo", () {
+    var todoController = TodoController();
+    var newId = Random().nextInt(10);
+    todoController
+        .addTodo(TodoModel(name: "Test 01", id: newId, isComplete: false));
+    expect(todoController.todoList.last.id, newId);
+  });
+  test("able to delete todo", () {
+    var todoController = TodoController();
+    todoController.todoList
+        .add(TodoModel(name: "Test 01", id: 0, isComplete: false));
+    todoController.deleteTodo(0);
+    expect(todoController.todoList.any((element) => element.id == 0), false);
+  });
 }
